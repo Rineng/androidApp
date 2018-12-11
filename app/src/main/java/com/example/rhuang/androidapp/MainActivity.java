cb8693facbbb820b8d5ca87849d5138b702cfb7c
Ronny Huang <rhuang@LT0136.T-mobile.com>
2018-12-10 22:08:51 -0500

    created cardViews testing

--- app/src/main/java/com/example/rhuang/androidapp/MainActivity.java
+++ app/src/main/java/com/example/rhuang/androidapp/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        recyclerView = (RecyclerView) findViewById(R.id.cardViews);
         mLayout = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(mLayout);
 
